900aca147f60d6ea649c8bf2cd3370adb34e1bae
Matias Vigil <matiasvigil@hotmail.com>
2025-07-21 14:55:20 -0300

    Agreamos funciones nuevas

---
+++ comandos_git.py
@@ -0,0 +1,13 @@
+# comandos para configurar nuestras identidad:
+
+#git config --global usaer.name "nombre y apellido"
+#git config --global usaer.email "nombre@example.com"
+
+#git init  Se una sola vez (crea carpeta .git)
+
+#Se usan cuando nosotros queramos:
+# git status  Muestra el estado del repo (archivos modificados, no agregados, etc.)
+# git log Muestra el historial de commits
+
+#git add . Agrega todos los archivos al repo (es aconsejable git add "nombre del arvhivo")
+#git commit -m "Mensaje" Crea un commit con los archivos agregados (etiqueta de info)
